e7c03f6d9246a64bd14d79ddcdaa9930f72e7486
swanandisarpale <swanandisarpale@gmail.com>
2026-06-02 22:21:45 +0530

    added file

--- fact.py
+++ fact.py
@@ -1,23 +1,22 @@
-
-
-
-def simple(p,r,t):
+def simple_intrest(p,r,t):
     si=p*r*t/100
     return si
 
-def fact(num):
-    if num==0 or num==1:
-     return 1
-    else:
-        return n*fact(n-1)
-    
-    p=float(input("p"))
-    r=float(input("r"))
-    t=float(input("r"))
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:  
+        return n*factorial(n-1)
 
-    result=simple(p,r,t)
-    print(result)
+p=float(input("eneter p"))
+r=float(input("enter r"))
+t=float(input("enter t"))
 
-    num=int(input("fact no"))
-    fatcto=fact(n)
-    print("fact",fact)
+result_si=simple_intrest(p,r,t)
+print("si",result_si)
+
+n=int(input("enetr num for fact"))
+fact=factorial(n)
+print("fact",fact)
+
+
