182c4ab6c98800b31d7476eceea0c1c787415d10
swanandisarpale <swanandisarpale@gmail.com>
2026-06-03 22:31:18 +0530

    added file

---
+++ object.py
@@ -0,0 +1,13 @@
+class stud:
+     def __init__(self,name,roll):
+          self.name=name
+          self.roll=roll
+
+     def display(self):
+             print("name",self.name)
+             print("roll",self.roll)
+             
+s1=stud("amit",56)
+s1.display()
+
+
