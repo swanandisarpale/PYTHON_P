ebdcb78e1d5c8d82dfed2e3c62a82f7aac37c19e
swanandisarpale <swanandisarpale@gmail.com>
2026-06-03 22:30:38 +0530

    added file

---
+++ list.py
@@ -0,0 +1,11 @@
+students=["sanu","ranu","sara","meera"]
+print("names of students")
+for names in students:
+    print(names)
+
+marks={"sanu":90,"ranu":89,"sara":87,"meera":78}
+high=max(marks.values())
+print("highest marks",high)
+
+
+
