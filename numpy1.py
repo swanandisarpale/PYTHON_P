ec80cb41d6d0ae543f29d73422fe8eb6010e614c
swanandisarpale <swanandisarpale@gmail.com>
2026-06-05 21:14:30 +0530

    added file

---
+++ numpy1.py
@@ -0,0 +1,13 @@
+import numpy1 as numpy1
+arr=numpy1.array([50,20,30,40])
+print("arr",arr)
+
+arr2=numpy1.array([12,13,14,15])
+add=arr+arr2
+print("addition",add)
+
+mean=numpy1.mean(arr)
+print("mean",mean)
+
+sorted=numpy1.sort(arr)
+print("sort",sorted)
